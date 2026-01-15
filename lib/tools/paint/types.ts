7a616578e19da9c1af9a3477a82883875cc68ce9
pauldaspden <93623797+pauldaspden@users.noreply.github.com>
2026-01-16 11:35:33 +1300

    Fix paint types module

---
+++ lib/tools/paint/types.ts
@@ -0,0 +1,24 @@
+export type PaintProjectInput = {
+  lengthM: number;
+  widthM: number;
+  heightM: number;
+  doorsCount: number;
+  windowsCount: number;
+  coats: 1 | 2 | 3;
+  coverageM2PerL: number;
+  wastePct: number;
+  includeCeiling: boolean;
+  pricePerL?: number;
+};
+
+export type PaintProjectResult = {
+  wallAreaM2: number;
+  ceilingAreaM2: number;
+  openingsAreaM2: number;
+  paintableAreaM2: number;
+  litresRaw: number;
+  litresWithWaste: number;
+  litresRounded: number;
+  recommendedCans: { sizeL: number; qty: number }[];
+  estimatedCost?: number;
+};
